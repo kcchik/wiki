8d7a9c6a2a7f4a38aa92fe8daa51961c39fbb4d7
Kevin Chik <kevin.c.chik@gmail.com>
2019-08-06 17:17:27 -0400

    File structure

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# wiki
+# Wiki
+
+### File navigation
+1. File tree on the left
+2. Top level files on the left, opening folders will show links to sub folders
+3. No folders
--- app.js
+++ app.js
@@ -1,75 +1,102 @@
 const http = require('http');
-const path = require('pathName');
+const path = require('path');
 const url = require('url');
 const fs = require('fs');
 const marked = require('marked');
 
-const walk = (pathName, done) => {
-  const root = {
-    pathName,
-    children: [],
-  };
-  let str = '<div>'
-  fs.readdir(pathName, (err, list) => {
+const walk = (root, done) => {
+  let str = '<div>';
+  const formattedRoot = root.replace(/^\.\/pages[/]?/, '/');
+  fs.readdir(root, (err, list) => {
     if (err) return done(err);
+
     let i = 0;
     (function next() {
       const file = list[i++];
       if (!file) return done(null, `${str}</div>`);
-      const childPath = `${pathName}/${file}`;
-      fs.stat(childPath, (err, stat) => {
-        if (stat && stat.isDirectory()) {
-          walk(childPath, (err, res) => {
-            root.children.push(res);
-            str += res;
-            next();
-          });
-        } else {
-          root.children.push({ pathName: file });
-          str += `<a>${file}</a>`;
+
+      const child = `${root}/${file}`;
+      fs.stat(child, (err, stat) => {
+        if (err) return done(err);
+
+        if (stat.isDirectory()) {
+          str += `<a href="${path.resolve(formattedRoot, file)}">${file}/</a>`;
           next();
-        };
+          // walk(child, (err, res) => {
+          //   if (err) return done(err);
+
+          // eslint-disable-next-line max-len
+          //   str += `<details><summary>/${file}</summary><div class="folder-content">${res}</div></details>`;
+          //   next();
+          // });
+        } else {
+          str += `<a href="${formattedRoot}?${file.replace(/\.[^/.]+$/, '')}">${file.replace(/\.[^/.]+$/, '')}</a>`;
+          next();
+        }
       });
-    })();
+    }());
+  });
+};
+
+const renderPage = (res, file, query) => {
+  fs.readFile('./index.html', 'utf8', (err, html) => {
+    if (err) throw err;
+
+    walk('./pages', (err, tree) => {
+      if (err) throw err;
+
+      html = html.replace('_tree_', tree);
+      if (query) {
+        fs.readFile(path.resolve(`./pages${file}`, `${query}.md`), 'utf8', (err, markdown) => {
+          if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(html.replace('_content_', marked(`# 404\n${err}`)));
+          } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(html.replace('_content_', marked(markdown)));
+          }
+        });
+      } else {
+        walk(`./pages${file}`, (err, tree) => {
+          if (err) throw err;
+
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(html.replace('_content_', `<h1>${file}</h1><a href="${path.dirname(file)}">../</a>${tree}`));
+        });
+      }
+    });
+  });
+};
+
+const renderOther = (res, file, ext) => {
+  const mime = {
+    '.css': 'text/css',
+  };
+  const type = mime[ext] || 'application/octet-stream';
+
+  fs.readFile(`.${file}`, (err, content) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end(err);
+    } else {
+      res.writeHead(200, { 'Content-Type': type });
+      res.end(content);
+    }
   });
 };
 
 http.createServer((req, res) => {
-  let pathName = url.parse(req.url).pathname;
-  if (pathName == '/') {
-    pathName = '/home';
-  };
-  const ext = String(path.extname(pathName));
-  if (!ext) {
-    walk('pages', (err, tree) => {
-      if (err) throw err;
-      fs.readFile('index.html', 'utf8', (err, html) => {
-        if (err) throw err;
-        html = html.replace('{{tree}}', tree);
-        fs.readFile(`pages${pathName}.md`, 'utf8', (err, markdown) => {
-          if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end(html.replace('{{content}}', marked('# 404\nThis page does not exist.')));
-          } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(html.replace('{{content}}', marked(markdown)));
-          };
-        });
-      });
-    });
-  } else {
-    const mimeTypes = {
-      '.css': 'text/css',
-    };
-    const type = mimeTypes[ext] || 'application/octet-stream';
-    fs.readFile(`.${pathName}`, (err, content) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end(err);
-      } else {
-        res.writeHead(200, { 'Content-Type': type });
-        res.end(content);
-      };
-    });
+  const parsedUrl = url.parse(req.url);
+  let { query } = parsedUrl;
+  const file = parsedUrl.pathname;
+  const ext = String(path.extname(file));
+  if (file === '/' && !query) {
+    query = 'home';
   }
-}).listen(8125);;
+
+  if (ext) {
+    renderOther(res, file, ext);
+  } else {
+    renderPage(res, file, query);
+  }
+}).listen(8125);

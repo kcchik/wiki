7718f5dba85806f6a9d70728f5642925a9d990d3
Kevin Chik <kevin.c.chik@gmail.com>
2019-08-13 17:14:34 -0400

    asd

--- pages/home.md
+++ pages/home.md
@@ -2,3 +2,8 @@
 I upload some of my documents and notes here.
 
 Mostly for my personal use, but if you found your way here, feel free to read.
+
+```
+this is a code snippet
+testing
+```
--- wiki.js
+++ wiki.js
@@ -14,7 +14,7 @@ const breadCrumbs = (file, query=null) => {
   if (query) {
     files = [...files, {
       url: `?${query}`,
-      name: decodeURI(query),
+      name: query,
     }];
   }
   return files.map((file) => `<a href="${file.url}">${file.name}</a>`).join('<span> > </span>');
@@ -85,12 +85,21 @@ const renderPage = (res, file, query) => {
 
       tree = `<h4>Menu</h4>${tree}`;
       if (query) {
-        fs.readFile(path.resolve(`./pages${file}`, `${query}.md`), 'utf8', (err, markdown) => {
+        fs.readFile(path.resolve(`./pages${file}`, `${query[0]}.md`), 'utf8', (err, markdown) => {
           if (err) {
             mount(404, `<h1>404</h1><p>${err}</p>`);
           } else {
             tree = `${listHeaders(marked(markdown))}${tree}`;
-            mount(200, `<p>${breadCrumbs(file, query)}</p>${marked(markdown)}`);
+            if (query[1] === 'edit') {
+              mount(200, `<form class="edit" action="${file}" method="POST">`
+                + `<input type="hidden" name="file" value="${query[0]}" />`
+                + `<p>${breadCrumbs(file, query[0])}</p>`
+                + '<input class="edit_button" type="submit" value="Save" />'
+                + `<textarea class="edit_textarea" name="content">${markdown}</textarea>`
+                + '</form>');
+            } else {
+              mount(200, `<p class="breadcrumbs">${breadCrumbs(file, query[0])}</p>${marked(markdown)}`);
+            }
           }
         })
       } else {
@@ -98,7 +107,7 @@ const renderPage = (res, file, query) => {
           if (err) {
             mount(404, `<h1>404</h1><p>${err}</p>`);
           } else {
-            mount(200, `<p>${breadCrumbs(file)}</p><h1>${file.split('/').pop()}</h1>${tree}`);
+            mount(200, `<p class="breadcrumbs">${breadCrumbs(file)}</p><h1>${file.split('/').pop()}</h1>${tree}`);
           }
         });
       }
@@ -112,16 +121,35 @@ const renderOther = (res, file, ext) => {
   });
 };
 
+const writePage = (req, res, file) => {
+  let data = '';
+  req.on('data', (chunk) => {
+    data += chunk;
+  });
+  req.on('end', () => {
+      const params = url.parse(`?${data}`, true).query
+      fs.writeFile(path.resolve(`./pages${file}`, `${params.file}.md`), params.content, (err) => {
+        if (err) throw err;
+        res.writeHead(301, { Location: `${file}?${params.file}` });
+        res.end();
+      });
+  });
+}
+
 const port = process.env.PORT || 8125;
 http.createServer((req, res) => {
   let { pathname, query } = url.parse(req.url);
   pathname = decodeURI(pathname);
-  query = query ? decodeURI(query) : null;
+  query = query ? decodeURI(query).split('&') : null;
   const ext = path.extname(pathname);
   if (pathname === '/' && !query) {
-    query = 'home';
+    query = ['home'];
   }
-  ext ? renderOther(res, pathname, ext) : renderPage(res, pathname, query);
+  if (req.method === 'POST') {
+    writePage(req, res, pathname);
+  } else {
+    ext ? renderOther(res, pathname, ext) : renderPage(res, pathname, query);
+  }
 }).listen(port);
 
 console.log(`http://127.0.0.1:${port}`);

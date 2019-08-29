2eca62474e28d1d4099351fc345788ce9ff96cd3
Kevin Chik <kevin.c.chik@gmail.com>
2019-08-29 10:43:26 -0400

    Fix build

--- build.js
+++ build.js
@@ -4,7 +4,9 @@ const db = new pg.Client({ connectionString: process.env.DATABASE_URL });
 
 db.connect((err) => {
   if (err) throw err;
-  db.query('CREATE TABLE pages(name text, directory text, content text)', () => {
+  db.query('CREATE TABLE pages(name text, directory text, content text)', (err) => {
+    if (err) return;
     db.query(`INSERT INTO pages VALUES ('home', '/', '# Home\nThis wiki is empty!')`);
   });
+  db.end();
 });
